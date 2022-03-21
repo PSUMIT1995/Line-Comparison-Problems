3cf8c142ff4b69bc14290b841fa68e955ee2f6f6
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-21 22:54:45 +0530

    Uc1 : Length Of The Line

---
+++ Line Comparison Problems/Line Comparison Problems/LineComparison.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Problems
+{
+    internal class LineComparison
+    {
+        ///variables
+        public double length_Of_Line;
+        ///method for calculation
+        public void Calc(double x1, double y1, double x2, double y2)
+        {
+            double X = Math.Pow(x2 - x1, 2.0);
+            double Y = Math.Pow(y2 - y1, 2.0);
+            length_Of_Line = Math.Sqrt(X + Y);
+        }
+    }
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            LineComparison lc = new LineComparison();
+            Console.WriteLine("Welcome To Line Comparison Problem ");
+            Console.WriteLine("enter x1,y1,x2,y2 values\n");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            lc.Calc(a, b, c, d);
+            Console.WriteLine("Line Length is = " + lc.length_Of_Line);
+        }
+
+    }
+}
+
